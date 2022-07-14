aeb41d3fd47dbe245c46e38706fb61bc019f77ef
eheath30 <elliot.heath@btinternet.com>
2022-07-14 19:07:19 +0100

    database updated, js functionality update in progress

--- client/js/allposts.js
+++ client/js/allposts.js
@@ -17,6 +17,7 @@ function appendResult(data){
 function appendResults(postData){
     const title = postData.title
     const alias = postData.alias
+    const id = postData.id
     const description = postData.description
     const date = postData.date
 
@@ -29,18 +30,19 @@ function appendResults(postData){
     targetDiv.setAttribute('class', "col-md-6")
 
     console.log(parentDiv)
+    console.log(postData)
 
-    targetDiv.innerHTML = 
+    targetDiv.innerHTML =
         `<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
             <div class="col p-4 d-flex flex-column position-static">
                 <strong class="d-inline-block mb-2 text-primary">${alias}</strong>
                 <h3 class="mb-0">${title}</h3>
                 <div class="mb-1 text-muted">${date}</div>
                 <p class="card-text mb-auto">${slicedArray}...</p>
-                <a href="#" class="stretched-link">Continue reading</a>
+                <a href="https://127.0.0.1:5502/client/index.html#post/${id}" class="mb-stretched-link">Continue reading</a>
             </div>
             <div class="col-auto d-none d-lg-block">
-                <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                <svg class="bd-placeholder-img" width="200" height="250"" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="44%" y="50%" fill="#eceeef" dy=".3em">🐼</text></svg>
             </div>
         </div>`
 
