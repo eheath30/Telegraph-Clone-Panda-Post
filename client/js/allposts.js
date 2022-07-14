1b29dacb48c6798add532cc784576c8e379cf13a
VAOkolo <vincentaokolo@outlook.com>
2022-07-14 20:22:33 +0100

    final commits

--- client/js/allposts.js
+++ client/js/allposts.js
@@ -33,7 +33,7 @@ function appendResults(postData){
     console.log(postData)
 
     targetDiv.innerHTML =
-        `<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        `<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-lg h-md-250 position-relative">
             <div class="col p-4 d-flex flex-column position-static">
                 <strong class="d-inline-block mb-2 text-primary">${alias}</strong>
                 <h3 class="mb-0">${title}</h3>
